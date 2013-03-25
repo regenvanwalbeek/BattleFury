1fbc4f02e9a7c9642f6af2667f2568dd44be0e98
W. Regen Van Walbeek <regenvanwalbeek@gmail.com>
2013-03-25 12:09:37 -0500

    Began adding Bepuphysics

---
+++ BattleFury/BattleFury/BattleFury/PhysicsEngine/Physics.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BEPUphysics;
+
+namespace BattleFury.PhysicsEngine
+{
+    // TODO should this extend Entity? Probably could...
+
+    public class Physics
+    {
+        private Space space;
+
+        public Physics()
+        {
+            space = new Space();   
+        }
+
+        public void Update()
+        {
+            space.Update();
+        }
+    }
+}
