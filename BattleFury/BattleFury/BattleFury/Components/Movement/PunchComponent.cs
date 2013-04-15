75dca85baf5f8ffdc14c096b3c03115dc31efe6d
W. Regen Van Walbeek <regenvanwalbeek@gmail.com>
2013-04-15 15:46:11 -0500

    started punching

---
+++ BattleFury/BattleFury/BattleFury/Components/Movement/PunchComponent.cs
@@ -0,0 +1,29 @@
+﻿
+using BattleFury.EntitySystem;
+using BattleFury.Entities;
+
+namespace BattleFury.Components.Movement
+{
+    public class PunchComponent : Component
+    {
+        public PunchComponent(Entity parent, Environment environment)
+            : base(parent, "PunchComponent")
+        {
+        
+        }
+
+        public override void Initialize()
+        {
+        }
+
+        public override void Start()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void Update(Microsoft.Xna.Framework.GameTime gameTime)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ BattleFury/BattleFury/BattleFury/Components/Movement/PunchableComponent.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BattleFury.EntitySystem;
+
+namespace BattleFury.Components.Movement
+{
+    public class PunchableComponent : Component
+    {
+
+        public PunchableComponent(Entity parent)
+            : base(parent, "PunchableComponent")
+        {
+        
+        }
+
+        public override void Initialize()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Start()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Update(Microsoft.Xna.Framework.GameTime gameTime)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
